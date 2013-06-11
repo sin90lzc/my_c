acd6f3b177e8329b8c31c3aa64cd829b19211b06
Tim Leung <lzcgame@gmail.com>
2013-06-11 16:53:07 +0800

    添加c/pre/includepath.c，练习include宏命令

---
+++ c/pre/includepath.c
@@ -0,0 +1,13 @@
+/**
+ *
+ * 练习使用include宏
+ *
+ * 这里需要添加环境变量export C_INCLUDE_PATH=$C_INCLUDE_PATH:../../include
+ *
+ */
+#include<stdio.h>
+#include<my.h>
+
+int main(int argc,char ** argv){
+	printf("LEN:%d\n",LEN);
+}
