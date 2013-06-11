0685885364592313ee0fa24c85f66c93a96ed4f5
Tim Leung <lzcgame@gmail.com>
2013-06-11 16:07:41 +0800

    operation.c:使用++对指针进行运算时的优先级。 	modified:   note.txt 	modified:   operation.c

--- c/pointer/operation.c
+++ c/pointer/operation.c
@@ -1,9 +1,14 @@
+/**
+ *
+ * ++运算符在指针运算操作中的优先级。可以看出是右结合性
+ */
 #include <stdio.h>
 //指针运算的区别
-void main(){
+int main(){
 	int a[]={1,2,3};
 	int * p1,* p2;
 	p1=p2=a;
 	printf("*p1++=%d\n",*p1++);
 	printf("*++p2=%d\n",*++p2);
+	return 0;
 }
