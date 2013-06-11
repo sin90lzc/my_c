6a73df099942bf6a0a981f70c04e01e38a059579
Tim Leung <lzcgame@gmail.com>
2013-06-11 16:09:28 +0800

    return_point.c:练习使用返回指向数组指针的函数

--- c/pointer/return_pointer.c
+++ c/pointer/return_pointer.c
@@ -1,14 +1,21 @@
+/**
+ *
+ * 练习使用返回指向数组指针的函数
+ */
 #include <stdio.h>
 
-void main(){
-int (*getArray())[4];
+int main(){
+	int (*getArray())[4];
 	int (*a)[4]=getArray();
 	//int a[4]=getArray();	
-	int i=0;
+	//int i=0;
 	printf("%d,%d,%d,%d\n",**a,*(*a+1),*(*a+2),*(*a+3));
+	return 0;
 }
+
 //返回一个指向数组的指针
 int (*getArray())[4]{
 	int a[][4]={{0,1,2,3}};
-	return a;
+	int (*p)[4]=a;
+	return p;
 }
